3b809ef02a4d9a5a496de0000f83c9fed63d52e5
hemalatharachakonda <hemalatharachakonda3@gmail.com>
2026-04-28 06:38:50 +0530

    Updated Group entity, DTO and application.properties

--- src/main/java/com/group/project/dto/GroupDTO.java
+++ src/main/java/com/group/project/dto/GroupDTO.java
@@ -11,51 +11,22 @@ public class GroupDTO {
     private LocalDate projectDeadline;
     private String requirements;
     
-    public String getName() {
-        return name;
-    }
+    // Getters and Setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
     
-    public void setName(String name) {
-        this.name = name;
-    }
+    public List<String> getStudents() { return students; }
+    public void setStudents(List<String> students) { this.students = students; }
     
-    public List<String> getStudents() {
-        return students;
-    }
+    public String getProjectTitle() { return projectTitle; }
+    public void setProjectTitle(String projectTitle) { this.projectTitle = projectTitle; }
     
-    public void setStudents(List<String> students) {
-        this.students = students;
-    }
+    public String getProjectDescription() { return projectDescription; }
+    public void setProjectDescription(String projectDescription) { this.projectDescription = projectDescription; }
     
-    public String getProjectTitle() {
-        return projectTitle;
-    }
+    public LocalDate getProjectDeadline() { return projectDeadline; }
+    public void setProjectDeadline(LocalDate projectDeadline) { this.projectDeadline = projectDeadline; }
     
-    public void setProjectTitle(String projectTitle) {
-        this.projectTitle = projectTitle;
-    }
-    
-    public String getProjectDescription() {
-        return projectDescription;
-    }
-    
-    public void setProjectDescription(String projectDescription) {
-        this.projectDescription = projectDescription;
-    }
-    
-    public LocalDate getProjectDeadline() {
-        return projectDeadline;
-    }
-    
-    public void setProjectDeadline(LocalDate projectDeadline) {
-        this.projectDeadline = projectDeadline;
-    }
-    
-    public String getRequirements() {
-        return requirements;
-    }
-    
-    public void setRequirements(String requirements) {
-        this.requirements = requirements;
-    }
+    public String getRequirements() { return requirements; }
+    public void setRequirements(String requirements) { this.requirements = requirements; }
 }
--- src/main/java/com/group/project/model/Group.java
+++ src/main/java/com/group/project/model/Group.java
@@ -28,6 +28,7 @@ public class Group {
         this.createdAt = LocalDateTime.now();
     }
     
+    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     
